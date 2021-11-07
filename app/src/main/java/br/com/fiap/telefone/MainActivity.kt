e9cd5bedf0345e80610127f1da72c3fbd3056816
Daniel Amaral <eumagnun@gmail.com>
2021-11-06 22:13:35 -0300

    first version

---
+++ app/src/main/java/br/com/fiap/telefone/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.fiap.telefone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import telefone.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
