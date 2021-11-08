a2bb704c8ff157e66008e2a1dc726aaa633d54a9
Daniel Amaral <eumagnun@gmail.com>
2021-11-08 18:31:30 -0300

    Demonstração ciclo de vida Activity

--- app/src/main/java/br/com/fiap/telefone/MainActivity.kt
+++ app/src/main/java/br/com/fiap/telefone/MainActivity.kt
@@ -2,11 +2,44 @@ package br.com.fiap.telefone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import telefone.R
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("ciclo-de-vida","onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("ciclo-de-vida","onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("ciclo-de-vida","onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("ciclo-de-vida","onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("ciclo-de-vida","onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("ciclo-de-vida","onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("ciclo-de-vida","onDestroy")
     }
 }
